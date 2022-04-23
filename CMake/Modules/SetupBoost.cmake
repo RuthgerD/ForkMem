2a19197ccb0ddcd63ea3872565e02d0705ac4381
Ruthger Dijt <ruthgerdijt@gmail.com>
2022-04-23 17:01:35 +0200

    boost source build

---
+++ CMake/Modules/SetupBoost.cmake
@@ -0,0 +1,89 @@
+#
+#  SetupBoost.cmake
+#  Copyright 2021-2022 ItJustWorksTM
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+
+# set (Boost_DEBUG True)
+set (BOOST_ENABLE_CMAKE True)
+
+if (DEFINED BOOST_SOURCE_ROOT)
+  set (boost_SOURCE_DIR "${BOOST_SOURCE_ROOT}")
+else ()
+  include (FetchContent)
+  # Can't reasonably default to Git until https://gitlab.kitware.com/cmake/cmake/-/issues/16144 is fixed
+  if (NOT BOOST_SOURCE_USE_GIT)
+    if (NOT WIN32)
+      FetchContent_Declare (Boost
+          URL "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz"
+          URL_HASH SHA256=94ced8b72956591c4775ae2207a9763d3600b30d9d7446562c552f0a14a63be7
+      )
+    else ()
+      FetchContent_Declare (Boost
+          URL "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.7z"
+          URL_HASH SHA256=090cefea470bca990fa3f3ed793d865389426915b37a2a3258524a7258f0790c
+      )
+    endif ()
+  else ()
+    FetchContent_Declare (Boost
+        GIT_REPOSITORY "https://github.com/boostorg/boost"
+        GIT_TAG "boost-1.78.0"
+        GIT_SHALLOW On
+        GIT_PROGRESS On
+    )
+  endif ()
+  FetchContent_GetProperties (Boost)
+  if (NOT boost_POPULATED)
+    message ("Downloading Boost")
+    FetchContent_Populate (Boost)
+
+    if (NOT EXISTS "${boost_SOURCE_DIR}/CMakeLists.txt")
+      file (DOWNLOAD "https://github.com/boostorg/boost/raw/boost-1.78.0/CMakeLists.txt" "${boost_SOURCE_DIR}/CMakeLists.txt")
+    endif ()
+    message ("Download complete")
+  endif ()
+endif ()
+
+set (PREV_BUILD_SHARED_LIBS "${BUILD_SHARED_LIBS}")
+set (BUILD_SHARED_LIBS False)
+
+set (PREV_CMAKE_POSITION_INDEPENDENT_CODE "${CMAKE_POSITION_INDEPENDENT_CODE}")
+set (CMAKE_POSITION_INDEPENDENT_CODE On)
+
+if (DEFINED BUILD_TESTING)
+  set (PREV_BUILD_TESTING "${BUILD_TESTING}")
+else ()
+  set (PREV_BUILD_TESTING "=UNDEFINED=")
+endif ()
+set (BUILD_TESTING Off)
+
+if (NOT DEFINED BOOST_EXCLUDE_LIBRARIES)
+  set (BOOST_EXCLUDE_LIBRARIES compute)
+endif ()
+string (TOLOWER "${SMCE_CXXRT_LINKING}" BOOST_RUNTIME_LINK)
+
+if (EXISTS "${boost_SOURCE_DIR}/boost")
+  include_directories (SYSTEM "${boost_SOURCE_DIR}")
+endif ()
+
+add_subdirectory ("${boost_SOURCE_DIR}" "${boost_BINARY_DIR}" EXCLUDE_FROM_ALL)
+
+if (BUILD_TESTING STREQUAL "=UNDEFINED=")
+  unset (BUILD_TESTING)
+else ()
+  set (BUILD_TESTING "${PREV_BUILD_TESTING}")
+endif ()
+set (CMAKE_POSITION_INDEPENDENT_CODE "${PREV_CMAKE_POSITION_INDEPENDENT_CODE}")
+set (BUILD_SHARED_LIBS "${PREV_BUILD_SHARED_LIBS}")
